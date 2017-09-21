3a65e3a7bae8d352a00521f5c04bdbba2de057a7
udi101 <udimaz@gmail.com>
2017-09-21 19:32:34 +0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+@NgModule({
+    imports: [],
+    exports: [],
+    declarations: [],
+    providers: []
+})
+export class AppRoutingModule {
+
+}
