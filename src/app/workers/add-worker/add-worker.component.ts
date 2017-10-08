7abcf04d193559067dba34e9c6739d597fea2500
udi101 <udimaz@gmail.com>
2017-10-09 00:15:28 +0300

    add debounceTime to Observable

--- src/app/workers/add-worker/add-worker.component.ts
+++ src/app/workers/add-worker/add-worker.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IWorker } from './../worker.interface';
 import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
+import 'rxjs/add/operator/debounceTime';
 
 @Component({
   selector: 'app-add-worker',
@@ -10,8 +11,17 @@ import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from
 export class AddWorkerComponent implements OnInit {
   workersForm: FormGroup;
   worker: IWorker;
+  salaryMessage: string;
+
+  private validationMessages: { [key: string]: string } = {
+    required: 'You must enter a salary',
+    range: 'The value entered does not follow the correct pattern'
+  };
+
   // firstName: FormControl = new FormControl();
   constructor(private formBuildier: FormBuilder) { }
+
+
 
   ngOnInit() {
     this.workersForm = this.formBuildier.group({
@@ -22,23 +32,47 @@ export class AddWorkerComponent implements OnInit {
         repeatEmail: ['', Validators.required],
       }, { validator: emailMatcer }),
       notification: 'email',
-      salary: [{ value: null, disabled: false }, checkSalary(100, 50000)]
+      salary: [{ value: null, disabled: false }, [Validators.required, checkSalary(100, 50000)]]
     });
 
-    // listening to valueChanges
-    this.workersForm.get('notification').valueChanges.subscribe(value => console.log(value));
+    // listening to email valueChanges
+    this.workersForm.get('notification').valueChanges.subscribe(val => {
+      switch (val) {
+        case 'text':
+          const wf = this.workersForm.controls.mailConfirmation;
+          wf.clearValidators();
+          this.workersForm.get('mailConfirmation.email').clearValidators();
+          wf.get('email').updateValueAndValidity();
+          break;
+        case 'email':
+          console.log('email');
+          break;
+      }
+    });
+
+    // Adding a watch for salary control
+    const salaryControl = this.workersForm.get('salary');
+    salaryControl.valueChanges.debounceTime(1000).subscribe(value =>
+      this.setMessage(salaryControl));
   }
 
+// Setting the error messages for salaryMessage
+  setMessage(c: AbstractControl): void {
+    this.salaryMessage = '';
+    if ((c.dirty || c.touched) && c.errors) {
+      this.salaryMessage = Object.keys(c.errors).map(key => this.validationMessages[key]).join(',');
+    }
+  }
 
   setValues() {
     this.workersForm.patchValue({
       firstName: 'Udi',
       lastName: 'Mazor',
-      salary: 25000
+      salary: 25000,
+      email: 'udimaz@gmail.com'
     });
   }
   save() {
-    // console.log('Saved: ' + this.workersForm);
   }
 
   submit() {
