919d9d90d356e18b679cb8bd46ae116d1b5ebce5
Udi Mazor <udi.mazor@graduway.com>
2018-04-22 11:39:06 +0300

    better *ngFor with trackBy

--- src/app/ui/ng-for/ng-for.component.ts
+++ src/app/ui/ng-for/ng-for.component.ts
@@ -1,22 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-ng-for',
   templateUrl: './ng-for.component.html',
   styleUrls: ['./ng-for.component.scss']
 })
-export class NgForComponent implements OnInit {
+export class NgForComponent implements OnInit, AfterViewInit {
   students: Array<{ id: number, name: string }> = [];
+  @ViewChildren('student') studentList: QueryList<ElementRef>;
   constructor() { }
 
   ngOnInit() {
     this.students =
-     [
-      { id: 1, name: 'Udi' },
-      { id: 2, name: 'Mazal' },
-      { id: 3, name: 'Alona' },
-      { id: 4, name: 'Ella' }
-    ];
+      [
+        { id: 1, name: 'Udi' },
+        { id: 2, name: 'Mazal' },
+        { id: 3, name: 'Alona' },
+        { id: 4, name: 'Ella' }
+      ];
+  }
+
+
+  ngAfterViewInit() {
+    console.log(this.studentList.first.nativeElement.innerHTML);
+    const studentListArray: Array<ElementRef> = this.studentList.toArray();
+    console.log(studentListArray[1].nativeElement.innerHTML);
+    this.studentList.changes.subscribe(s => console.log(s));
   }
 
   updateList() {
